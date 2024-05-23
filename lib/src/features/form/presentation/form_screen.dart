5e56f3263d53c976256387269d3bf3fb7bb3c433
Vad3s <167074233+Vad3s@users.noreply.github.com>
2024-05-23 12:19:03 +0200

    Fertig + Leerzeichen nicht erlaubt

--- lib/src/features/form/presentation/form_screen.dart
+++ lib/src/features/form/presentation/form_screen.dart
@@ -44,12 +44,34 @@ class FormScreen extends StatelessWidget {
   }
 
   String? validateEmail(String? input) {
-    // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return 'E-Mail darf nicht leer sein';
+    }
+    if (input.contains(' ')) {
+      return 'E-Mail darf keine Leerzeichen enthalten';
+    }
+    if (input.length <= 6) {
+      return 'E-Mail muss mehr als 6 Zeichen haben';
+    }
+    if (!input.contains('@')) {
+      return 'E-Mail muss ein "@" enthalten';
+    }
+    if (!(input.endsWith('.com') || input.endsWith('.de'))) {
+      return 'E-Mail muss mit ".com" oder ".de" enden';
+    }
     return null;
   }
 
   String? validatePw(String? input) {
-    // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return 'Passwort darf nicht leer sein';
+    }
+    if (input.contains(' ')) {
+      return 'Passwort darf keine Leerzeichen enthalten';
+    }
+    if (input.length < 6 || input.length > 12) {
+      return 'Passwort muss zwischen 6 und 12 Zeichen lang sein';
+    }
     return null;
   }
 }
